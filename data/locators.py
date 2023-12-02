f07d2996322f1eeebdac755d54fa571f993e8e27
DmanYV <dima-xxx@mail.ru>
2023-12-02 11:22:39 +0300

    - Добавлены новые локаторы - Дописаны новые функции

--- data/locators.py
+++ data/locators.py
@@ -15,5 +15,15 @@ class LoginPageLocators:
     FIELD_USERNAME = (By.XPATH, './/input[@name="username"]')
     # Поле ввода 'Пароль'
     FIELD_PASSWORD = (By.XPATH, './/input[@name="password"]')
+    # Текст валидации поля 'Имя пользователя'
+    VALIDATION_MESSAGE_USERNAME_FIELD = (
+        By.XPATH, '//label[contains(.,"Введи e-mail, никнейм или номер телефона, указанные при регистрации")]')
+    # Текст валидации поля 'Пароль'
+    VALIDATION_MESSAGE_PASSWORD_FIELD = (By.XPATH, '//label[contains(.,"Введи пароль")]')
     # Кнопка 'Войти'
     BUTTON_SIGN_IN = (By.XPATH, './/button[text()="Войти"]')
+    # Кнопка 'Крести\Закрыть'
+    BUTTON_BACK = (By.XPATH, './/button[contains(@class, "header_back")]')
+    # Кнопка 'Забыл(-а) пароль?'
+    BUTTON_FORGO_PASSWORD = (By.XPATH, './/div[text()="Забыл(-а) пароль?"]')
+    SPINNER = (By.XPATH, './/div[@class="spinner_rotating-plane__1g-WO"]')
--- model/pages/login_page.py
+++ model/pages/login_page.py
@@ -10,14 +10,26 @@ class LoginPage(BasePage):
     PAGE_URL = Links.LOGIN_PAGE
 
     def field_username_input(self, text: str):
-        elem = self.find_element(LoginPageLocators.FIELD_USERNAME)
-        elem.send_keys(text)
+        element = self.find_element(LoginPageLocators.FIELD_USERNAME)
+        element.send_keys(text)
 
     def field_password_input(self, text: str):
-        elem = self.find_element(LoginPageLocators.FIELD_PASSWORD)
-        elem.send_keys(text)
+        element = self.find_element(LoginPageLocators.FIELD_PASSWORD)
+        element.send_keys(text)
+
+    def validation_message_username_field(self, text, wait_time=10):
+        WebDriverWait(self.driver, wait_time).until(EC.text_to_be_present_in_element(
+            LoginPageLocators.VALIDATION_MESSAGE_USERNAME_FIELD, text))
 
     def button_signin_click(self):
-        elem = self.find_element(LoginPageLocators.BUTTON_SIGN_IN)
-        self.do_click(elem)
+        element = self.find_element(LoginPageLocators.BUTTON_SIGN_IN)
+        self.do_click(element)
+
+    def button_back_click(self):
+        element = self.find_element(LoginPageLocators.BUTTON_BACK)
+        self.do_click(element)
+
+    def button_forgot_password_click(self):
+        element = self.find_element(LoginPageLocators.BUTTON_FORGO_PASSWORD)
+        self.do_click(element)
 
