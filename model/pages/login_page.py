4a43ce4eb2c9c92c65ccd52bff008152dca00020
DmanYV <dima-xxx@mail.ru>
2024-01-26 23:27:13 +0300

    - Pip8

--- model/pages/login_page.py
+++ model/pages/login_page.py
@@ -9,30 +9,31 @@ from data.locators import LoginPageLocators
 class LoginPage(BasePage):
     PAGE_URL = Links.LOGIN_PAGE
 
-    def input_username_field(self, login: str):
+    def field_username_input(self, login: str):
         element = self.find_element(LoginPageLocators.FIELD_USERNAME)
         element.send_keys(login)
 
-    def input_password_field(self, password: str):
+    def field_password_input(self, password: str):
         element = self.find_element(LoginPageLocators.FIELD_PASSWORD)
         element.send_keys(password)
 
-    def validation_message_username_field(self, text, wait_time=10):
+    def field_username_validation_message(self, text, wait_time=10):
         WebDriverWait(self.driver, wait_time).until(EC.text_to_be_present_in_element(
             LoginPageLocators.VALIDATION_MESSAGE_USERNAME_FIELD, text))
 
-    def click_signin_button(self):
+    def button_signin_click(self):
         element = self.find_element(LoginPageLocators.BUTTON_SIGN_IN)
         self.do_click(element)
 
-    def click_back_button(self):
+    def button_back_click(self):
         element = self.find_element(LoginPageLocators.BUTTON_BACK)
         self.do_click(element)
 
-    def click_forgot_password_button(self):
+    def button_forgot_password_click(self):
         element = self.find_element(LoginPageLocators.BUTTON_FORGO_PASSWORD)
         self.do_click(element)
 
     def check_popup_auth_error_visible(self, text, wait_time=10):
-        WebDriverWait(self.driver, wait_time).until(EC.text_to_be_present_in_element(
+        text_ = WebDriverWait(self.driver, wait_time).until(EC.text_to_be_present_in_element(
             LoginPageLocators.POPUP_AUTH_ERROR, text))
+        assert text_ == text
