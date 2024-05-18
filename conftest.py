fb4326f2db0327e01bf0ddacc045717be50e02ba
DmanYV <dima-xxx@mail.ru>
2024-05-18 22:51:36 +0300

    - Добавлен докер

--- conftest.py
+++ conftest.py
@@ -6,11 +6,11 @@ from config import attach
 @pytest.fixture(autouse=True, scope='function')
 def driver(request):
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--window-size=1920,1080')  # размер окна is 1920x1080
+    # chrome_options.add_argument('--window-size=1920,1080')  # размер окна is 1920x1080
     # chrome_options.add_argument('--no-sandbox')  # no sandbox
     chrome_options.add_argument('--disable-dev-shm-usage')  # overcome limited resource problems
     # chrome_options.add_argument('--incognito')  # режим инкогнито
-    # chrome_options.add_argument('--headless')  # режим без UI
+    chrome_options.add_argument('--headless')  # режим без UI
     # chrome_options.add_argument('--ignore-certificate-errors')  # режим игнорирования сертификата
     driver = webdriver.Chrome(options=chrome_options)
     request.cls.driver = driver
