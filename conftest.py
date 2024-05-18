d5fa6c60b388f7a5b1352f739d9ca9bd870f9871
DmanYV <dima-xxx@mail.ru>
2024-05-18 23:21:54 +0300

    - настройка CI GitHub Pages

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from config import attach
 def driver(request):
     chrome_options = webdriver.ChromeOptions()
     # chrome_options.add_argument('--window-size=1920,1080')  # размер окна is 1920x1080
-    # chrome_options.add_argument('--no-sandbox')  # no sandbox
+    chrome_options.add_argument('--no-sandbox')  # no sandbox
     chrome_options.add_argument('--disable-dev-shm-usage')  # overcome limited resource problems
     # chrome_options.add_argument('--incognito')  # режим инкогнито
     chrome_options.add_argument('--headless')  # режим без UI
