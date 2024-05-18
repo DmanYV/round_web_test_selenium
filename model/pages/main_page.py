7452a0573ba75ae286f863e48dbf74acc00684de
DmanYV <105636957+DmanYV@users.noreply.github.com>
2024-05-18 21:19:44 +0000

    Deploying to gh-pages from @ DmanYV/round_web_test_selenium@4ade0a254767be793d763ec5cc4ac9f78999e18f 🚀

--- model/pages/main_page.py
+++
@@ -1,6 +0,0 @@
-from base.base_page import BasePage
-from config.links import Links
-
-
-class MainPage(BasePage):
-    PAGE_URL = Links.MAIN_PAGE
--- model/pages/rubric_page.py
+++
@@ -1,6 +0,0 @@
-from base.base_page import BasePage
-from config.links import Links
-
-
-class RubricPage(BasePage):
-    PAGE_URL = Links.RUBRIC_PAGE
