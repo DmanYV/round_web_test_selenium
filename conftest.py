3fd0739f60b8ee36d98c9e94f6bc94c314e89e59
DmanYV <dima-xxx@mail.ru>
2024-07-04 20:05:12 +0300

    - Создан тест кейс регистрации нового пользователя

--- conftest.py
+++ conftest.py
@@ -9,10 +9,10 @@ from base.app_locators import *
 def driver(request):
     chrome_options = Options()
     chrome_options.add_argument('--window-size=1920,1080')  # размер окна is 1920x1080
-    # chrome_options.add_argument('--no-sandbox')  # no sandbox
+    chrome_options.add_argument('--no-sandbox')  # no sandbox
     chrome_options.add_argument('--disable-dev-shm-usage')  # overcome limited resource problems
     # chrome_options.add_argument('--incognito')  # режим инкогнито
-    # chrome_options.add_argument('--headless')  # режим без UI
+    chrome_options.add_argument('--headless')  # режим без UI
     # chrome_options.add_argument('--ignore-certificate-errors')  # режим игнорирования сертификата
     driver = webdriver.Chrome(options=chrome_options)
     request.cls.driver = driver
