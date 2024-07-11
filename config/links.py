520658c87ee6791547e2e054112735d9f8b6dbfe
DmanYV <dima-xxx@mail.ru>
2024-07-11 23:27:38 +0300

    - Добавлена ссылка на страницу сброса пароля

--- config/links.py
+++ config/links.py
@@ -5,6 +5,7 @@ class Links:
     AUTHORIZATION_PAGE = f'{HOST}/authorization'
     LOGIN_PAGE = f'{HOST}/login'
     JOIN_PAGE = f'{HOST}/join'
+    RESET_PASSWORD_PAGE = f'{HOST}/reset-password'
     MAIN_PAGE = f'{HOST}/'
     RUBRIC_PAGE = f'{HOST}/rubric'
 
