409d5208d2a6d094f5cf3a42dd87f357f274145d
DmanYV <dima-xxx@mail.ru>
2024-07-22 23:18:03 +0300

    - Автоматизирован тест кейс на проверку просмотра проектов в своем профиле

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -21,3 +21,16 @@ class TestProfile(BaseTest):
         with allure.step('Проверить, что отображается никнейм Aleska'):
             element = elements['Профиль пользователя']
             self.assertion.text_in_element(element['Никнейм пользователя'], expected_text='Aleska')
+
+    @allure.title('Проверка возможности просмотреть все свои проекты в профиле')
+    @allure.description('Проверяется, что при открытие пользователя отображаются проекты в количестве > 0')
+    @allure.severity('Critical')
+    @pytest.mark.regression
+    def test_open_user_profile(self, elements, login_to_app):
+        with allure.step('Нажать на кнопку профиль'):
+            element = elements['Панель навигации']
+            self.app.do_click(element['Кнопка профиль'])
+
+        with allure.step('Проверить, что количество проектов > 0'):
+            element = elements['Профиль пользователя']
+            self.assertion.length_elements(element['Список проектов'])
