c7b0da48285305a93dba9cb5ef7afc7910ee2ecc
DmanYV <dima-xxx@mail.ru>
2024-08-12 23:38:13 +0300

    - Добавлены аннотации к методам для улучшения читаемости

--- model/pages/subscription_page.py
+++ model/pages/subscription_page.py
@@ -13,6 +13,10 @@ class SubscriptionPage(BasePage):
         self.app = App(driver)
 
     def subscribe_user(self):
+        """
+        Подписка на первого пользователя в списке
+
+        """
         self.app.profile_button_click()
         self.do_click(config.locators.ProfilePageLocators.locators['Подписки'])
         self.do_click(config.locators.SubscriptionPageLocators.locators['Все пользователи'])
@@ -20,12 +24,22 @@ class SubscriptionPage(BasePage):
         self.do_click(elem)
 
     def unsubscribe_user(self):
+        """
+        Отписка от перового пользователя в списке
+
+        """
         self.app.profile_button_click()
         self.do_click(config.locators.ProfilePageLocators.locators['Подписки'])
         elem = self.find_element(config.locators.SubscriptionPageLocators.locators['Отписка от первого пользователя'])
         self.do_click(elem)
 
     def search_by_username(self, username):
+        """
+        Поиск пользователя в списке по имени и переходе в его профиль
+
+        :param username: Имя пользователя
+
+        """
         user = self.find_element(
             (By.XPATH, f'//div[contains(@class,"user-block_wrapper-name")][contains(.,"{username}")]'))
         self.do_click(user)
