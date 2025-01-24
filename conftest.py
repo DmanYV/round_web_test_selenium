c4b785c1b64dbc06cdc534e40fc4a77a62dd95fe
DmanYV <dima-xxx@mail.ru>
2025-01-24 22:33:23 +0300

    Создана фикстура подключения к дб

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import allure
 import pytest
+import psycopg
+
 from selenium import webdriver
 from selenium.common import TimeoutException
 
@@ -8,7 +10,7 @@ from config import attach
 from selenium.webdriver.chrome.options import Options
 from base.app_locators import *
 from model.api.authorization import *
-from settings import User
+from settings import User, DB
 
 
 @pytest.fixture(autouse=True, scope='function')
@@ -46,7 +48,7 @@ def elements(request) -> dict:
 @allure.title('Авторизация пользователя в системе')
 @allure.step('Авторизация пользователя в системе')
 def login_to_app(request, driver):
-    """ Логин в систему """
+    """ Фикстура Логина в систему """
     login_page = ApiAuthorization(driver)
     login_page.user(User.LOGIN, User.PASSWORD)
     try:
@@ -56,11 +58,44 @@ def login_to_app(request, driver):
         element = login_page.find_element(
             config.locators.MainPageLocators.locators['Листай вниз и найди свои интересы'])
         login_page.do_click(element)
-    # try:
-    #     login_page.find_element(config.locators.MainPageLocators.locators['Листай вниз и найди свои интересы'])
-    # except TimeoutException:
-    #     element = login_page.find_element(config.locators.RubricPageLocators.locators['Кнопка выберу потом'])
-    #     login_page.do_click(element)
-    # element = login_page.find_element(
-    #     config.locators.MainPageLocators.locators['Листай вниз и найди свои интересы'])
-    # login_page.do_click(element)
+
+
+@pytest.fixture(scope="module")
+@allure.title('Установка соединения с БД')
+@allure.step('Установка соединения с БД')
+def db_connection(request):
+    """
+    Фикстура для установки соединения с базой данных.
+
+    Args:
+        request: объект запроса pytest.
+
+    Returns:
+        connection: объект соединения с базой данных.
+
+    Raises:
+        Exception: если возникает ошибка при подключении к базе данных.
+    """
+    connection = None
+    try:
+        connection = psycopg.connect(
+            dbname=DB.DBNAME,
+            user=DB.DBUSER,
+            password=DB.DBPASSWORD,
+            host=DB.DBHOST,
+            port=DB.DBPORT
+        )
+        print('Соединение с БД установлено успешно')
+
+        def fin():
+            if connection:
+                connection.close()
+                print('Соединение с БД закрыто')
+
+        request.addfinalizer(fin)
+        return connection
+    except Exception as error:
+        print(f'Ошибка при подключении к БД: {error}')
+        if connection:
+            connection.close()
+            print('Соединение с БД закрыто')
