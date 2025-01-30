1771c9adbfa5f4c09bf5aa26333b9e7a0dcd54c9
DmanYV <dima-xxx@mail.ru>
2025-01-30 23:10:00 +0300

    Создан метод клика по кнопке избранное

--- model/pages/challenge_page.py
+++ model/pages/challenge_page.py
@@ -1,6 +1,10 @@
+import config.locators
+from base.base_assertion import Assertion
+
 from base.base_page import BasePage
 from model.elements.app import App
 from config.links import Links
+
 
 
 class ChallengePage(BasePage):
@@ -9,3 +13,13 @@ class ChallengePage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.app = App(driver)
+        self.assertion = Assertion(driver)
+        self.element = config.locators.ChallengePageLocators.locators
+
+    def click_favorites_button(self):
+        """
+        Клик по кнопке избранное
+
+        """
+        self.assertion.is_elem_displayed(self.element['Проекты участников'])
+        self.do_click(self.element['Кнопка избранное'])
